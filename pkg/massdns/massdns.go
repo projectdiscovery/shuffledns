bdd8bebd93d024dd8727d8121be6a1ced41dc6e4
six2dez <alexis6307@gmail.com>
2021-04-14 13:06:37 +0200

    Update massdns.go

--- pkg/massdns/massdns.go
+++ pkg/massdns/massdns.go
@@ -46,6 +46,8 @@ type Config struct {
 var excellentResolvers = []string{
 	"8.8.8.8",
 	"8.8.4.4",
+	"208.67.222.222",
+	"208.67.220.220",
 }
 
 // New returns a new massdns client for running enumeration
