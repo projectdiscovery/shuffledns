7840b26bba93514a531687a2c5d526544d7e50cf
sandeep <8293321+ehsandeep@users.noreply.github.com>
2023-05-07 17:25:11 +0530

    version update

--- pkg/runner/banners.go
+++ pkg/runner/banners.go
@@ -9,19 +9,16 @@ const banner = `
        __        ________        __       
   ___ / /  __ __/ _/ _/ /__  ___/ /__ ___
  (_-</ _ \/ // / _/ _/ / -_)/ _  / _ \(_-<
-/___/_//_/\_,_/_//_//_/\__/ \_,_/_//_/___/ v1.0.8
+/___/_//_/\_,_/_//_//_/\__/ \_,_/_//_/___/
 `
 
 // version is the current version of shuffledns
-const version = `v1.0.8`
+const version = `v1.0.9`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
 	gologger.Print().Msgf("%s\n", banner)
 	gologger.Print().Msgf("\t\tprojectdiscovery.io\n\n")
-
-	gologger.Print().Msgf("Use with caution. You are responsible for your actions\n")
-	gologger.Print().Msgf("Developers assume no liability and are not responsible for any misuse or damage.\n")
 }
 
 // GetUpdateCallback returns a callback function that updates shuffledns
